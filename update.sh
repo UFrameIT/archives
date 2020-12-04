e257ccdc226f0e84edd0607b90e38fe6d476a215
ComFreek <comfreek@outlook.com>
2020-12-04 22:38:28 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# when editing, reflect changes in `update.ps1`, too!
 git pull && git pull --recurse-submodules && git submodule foreach git checkout devel && git submodule foreach git pull
