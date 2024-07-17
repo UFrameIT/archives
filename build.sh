4aefa2356ba88885d3b83ed1742c618ca4bebe70
bessw <56679598+bessw@users.noreply.github.com>
2024-07-17 18:01:17 +0200

    Workaround until MMTs run-file script is fixed, see https://github.com/UniFormal/MMT/pull/589

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -euxo pipefail
 
-../MMT/deploy/run-file build.msl
-#java -Xmx8192m -cp "../MMT/deploy/mmt.jar" info.kwarc.mmt.api.frontend.Run :file build.msl
+#../MMT/deploy/run-file build.msl
+java -Xmx8192m -cp "../MMT/deploy/mmt.jar" info.kwarc.mmt.api.frontend.Run :file build.msl
